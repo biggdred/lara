d2e378871de12364024fc8de7c698cafc5cd9b57
biggdred <allijgibs@gmail.com>
2017-07-24 12:33:14 +0300

    creating my page ,her model and controller and view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,16 +9,13 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-old version:
-$routs::get('/', function(){
-    return view('welcome');
-}
+
 */
 
 Route::get('/', 'WelcomeController@index');
 
 //mypage
-Routs::get('/','PageContoller@Indexx');
+Route::get('/page', 'PageController@index');
 
 Route::get('home', 'HomeController@index');
 
